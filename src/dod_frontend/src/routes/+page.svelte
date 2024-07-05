454b8d6aecf590b9533b24fa654d6a10ff440be1
RikiReal <rijad@tutanota.de>
2024-07-05 18:53:48 +0200

    Add login with Internet Identity

--- src/dod_frontend/src/routes/+page.svelte
+++ src/dod_frontend/src/routes/+page.svelte
@@ -1,3 +1,34 @@
+<script lang="ts">
+  import { AuthClient } from "@dfinity/auth-client";
+  import { onMount } from "svelte";
+
+  onMount(async () => {
+    const identityProvider =
+      process.env.DFX_NETWORK === "local"
+        ? "http://be2us-64aaa-aaaaa-qaabq-cai.localhost:4943/"
+        : "identity.ic0.app";
+
+    const loginButton = document.getElementById(
+      "login-button",
+    ) as HTMLButtonElement;
+    loginButton.onclick = async (e) => {
+      e.preventDefault();
+      const authClient = await AuthClient.create();
+      await new Promise<void>((resolve, reject) => {
+        authClient.login({
+          onSuccess: () => {
+            console.log("Logged in");
+            location.href = "/home";
+            resolve();
+          },
+          onError: reject,
+          identityProvider,
+        });
+      });
+    };
+  });
+</script>
+
 <main
   class="flex min-h-screen min-w-full flex-col items-center justify-center gap-20 bg-red-300"
 >
@@ -8,10 +39,9 @@
       >Done</span
     >
   </div>
-  <a href="/home">
-    <button
-      class="rounded-full border-2 border-black bg-purple-500 px-10 py-3 text-5xl hover:bg-purple-600"
-      >Login!</button
-    >
-  </a>
+  <button
+    id="login-button"
+    class="rounded-full border-2 border-black bg-purple-500 px-10 py-3 text-5xl hover:bg-purple-600"
+    >Login!</button
+  >
 </main>
